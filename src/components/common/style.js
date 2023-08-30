fb501a41fcb9e194c071cb0ea3c2bae0112c7ae4
savazyggg <younji0715@gmail.com>
2023-08-31 01:50:21 +0900

    [design]: content section style added

--- src/components/common/style.js
+++ src/components/common/style.js
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { opacity } from "./core";
 
 export const SContainer = styled.div`
   width: 100%;
@@ -6,4 +7,12 @@ export const SContainer = styled.div`
   flex-direction: column;
   overflow-y: scroll;
   height: calc(100vh - 35px);
+  ::-webkit-scrollbar {
+    width: 5px;
+    background-color: transparent;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: rgba(176, 168, 185, ${opacity.heavy});
+  }
 `;
